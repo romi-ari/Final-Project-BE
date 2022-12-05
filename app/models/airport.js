766eb537bbba5ca7bc9166b1933a87058048b01d
ronnyfebriansaputra12 <ronnyfebriansaputra04@gmail.com>
2022-12-06 01:40:37 +0700

    CRUD table flight

--- app/models/airport.js
+++ app/models/airport.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Airport.hasMany(models.Flight,{
-        foreignKey : 'form_airport_id'
+        foreignKey : 'from_airport_id'
       })
     }
   }
