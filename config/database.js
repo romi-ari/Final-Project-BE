113018869b2e1b03d1696965da1bce39370fe582
ronnyfebriansaputra12 <ronnyfebriansaputra04@gmail.com>
2022-12-06 12:52:53 +0700

    include list flight plane airport

--- config/database.js
+++ config/database.js
@@ -52,3 +52,36 @@ module.exports = {
   },
   db
 };
+
+
+
+// const {
+//   DB_USERNAME = "root",
+//   DB_PASSWORD = "",
+//   DB_HOST = "127.0.0.1",
+//   DB_NAME = "db_fp",
+// } = process.env;
+
+// module.exports = {
+//   development: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: `${DB_NAME}_development`,
+//     host: DB_HOST,
+//     dialect: "mysql",
+//   },
+//   test: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: `${DB_NAME}_test`,
+//     host: DB_HOST,
+//     dialect: "mysql",
+//   },
+//   production: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: `${DB_NAME}_production`,
+//     host: DB_HOST,
+//     dialect: "mysql",
+//   },
+// };
